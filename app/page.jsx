e5b5a98d4a0271194f765fe5eae56b3e61ee0b6b
JahanZaib Sultan <jazykhan3@gmail.com>
2024-02-04 07:47:05 +0500

    hot fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,15 @@
-import About from "@/components/About";
-import Achievments from "@/components/Achievements";
-import Education from "@/components/Education";
-import Experience from "@/components/Experience";
-import Hero from "@/components/Hero";
-import Interests from "@/components/Interests";
-import Languages from "@/components/Languages";
-import Projects from "@/components/Projects";
-import Recommendations from "@/components/Recommendations";
-import Skills from "@/components/Skills";
-import Volunteering from "@/components/Volunteering";
+import About from '../components/About';
+import Achievments from "../components/Achievements";
+import Education from "../components/Education";
+import Experience from "../components/Experience";
+import Hero from "../components/Hero";
+import Interests from "../components/Interests";
+import Languages from "../components/Languages";
+import Projects from "../components/Projects";
+import Recommendations from "../components/Recommendations";
+import Skills from "../components/Skills";
+import Volunteering from "../components/Volunteering";
+
 
 
 export default function Home() {
